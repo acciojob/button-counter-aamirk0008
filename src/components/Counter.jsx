448a5fb568851352bb24f139f3d21b0996f7e4a0
Sheikh Aamir <113338901+aamirk0008@users.noreply.github.com>
2025-08-30 02:09:28 +0530

    Update Counter.jsx

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,1 +1,18 @@
+import { useState } from 'react'
 
+const Counter = () => {
+  return (
+    const [count, setCount] = useState(0)
+
+    const handleClick = () => {
+      setCount(count++)
+    }
+    <div>
+    <p>Button Clicked {count} times</p>
+        <button onClick={handleClick}>Click me</button>
+    </div>
+    
+  )
+}
+
+export default Counter
